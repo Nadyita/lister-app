d2d5187deb23ae761cc0b59c937f4503d69229a4
Nadyita <nadyita@hodorraid.org>
2025-10-28 18:10:52 +0100

    Release v0.0.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "xyz.travitia.lister"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "0.0.15"
+        versionCode = 15
+        versionName = "0.0.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
